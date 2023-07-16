0fe860f709ebc391acb1c603d4cbfefe5750e7c6
muasya <muasyamumina@gmail.com>
2023-07-16 14:21:28 +0300

    completed dom modifying lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+// Remove the #main node
+document.getElementById('main').remove();
+
+// Create a new header with id and textContent
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'YOUR-NAME is the champion';
+
+// Append newHeader to the body
+document.body.appendChild(newHeader);
